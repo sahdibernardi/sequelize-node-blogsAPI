81b1e01a2a7903b82fe2113afa526f040ed899ee
Sahra Di Bernardi <sah.dibernardi@gmail.com>
2023-03-26 17:42:42 -0300

    fix: token verification

--- src/auth/validateJWT.js
+++ src/auth/validateJWT.js
@@ -1,30 +1,26 @@
 const jwt = require('jsonwebtoken');
 
-const { UserService } = require('../services');
-
-const secret = process.env.JWT_SECRET || 'seusecretdetoken';
+const secret = process.env.JWT_SECRET || 'mySecretToken';
 
 const tokenValidation = async (req, res, next) => {
     const token = req.header('Authorization');
 
     if (!token) {
-        return res.status(401).json({ error: 'Token não encontrado' });
+        return res.status(401).json({ error: 'Token not found' });
     }
 
     try {
         const decoded = jwt.verify(token, secret);
-        
-    const user = await UserService.getByUserId(decoded.data.userId);
 
-    if (!user) {
-        return res.status(401).json({ message: 'Erro ao procurar usuário do token.' });
+    if (!decoded) {
+        return res.status(401).json({ message: 'Expired or invalid token' });
     }
 
-    req.user = user;
+    req.data = decoded.data;
 
     next();
     } catch (err) {
-        return res.status(401).json({ message: err.message });
+        return res.status(401).json({ message: 'Expired or invalid token' });
     }
 };
 
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { tokenValidation } = require('../auth');
 const { userController } = require('../controllers');
 const validateEmail = require('../middlewares/validateEmail');
 const validateName = require('../middlewares/validateName');
@@ -7,5 +8,7 @@ const validatePassword = require('../middlewares/validatePassword');
 const userRouter = express.Router();
 
 userRouter.post('/', validateName, validateEmail, validatePassword, userController.createUser);
+
+userRouter.get('/', tokenValidation, userController.functionName);
 
 module.exports = userRouter;
